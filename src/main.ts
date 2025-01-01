47ad015ec9186a560cc1cc795963545bfa708e2d
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2025-01-02 00:02:40 +0200

    feat: Enable detailed logging in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'], // Enable all log levels
+  });
   // Configure CORS
   app.enableCors({
     origin: 'http://localhost:8081', // Replace with your Expo development server URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
   await app.listen(3000);
 }
 bootstrap();
