21aba9ec35c27b770d6bef3d3ddd51c805cc5dba
Blad3z <51128241+CallMeBlad3z@users.noreply.github.com>
2025-01-02 00:01:42 +0200

    feat: Add global configuration and email, config modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountryModule } from './country/country.module';
+import { ConfigModule } from '@nestjs/config';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [HttpModule, CountryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HttpModule,
+    CountryModule,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
