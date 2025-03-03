85275e297999938d7714193a80c5bfe30a00067a
Blad3z <santtu366@live.com>
2025-03-03 23:46:18 +0200

    feat: Enhance EmailService with SSL configuration, connection timeout, and new contact us email method

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -10,10 +10,14 @@ export class EmailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT, 10),
+      secure: true, // Use SSL
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
+      connectionTimeout: 25000, // 25 seconds
+      logger: false, // Enable logging
+      debug: false, // Enable debugging
     });
   }
 
@@ -22,10 +26,31 @@ export class EmailService {
     this.logger.log('sendBugReport called with:', { fromEmail, subject, bugReport }); // Log the input
 
     const mailOptions = {
-      from: `"${fromEmail}" <${process.env.SMTP_USER}>`, // Ensure the "From" header is valid
-      to: 'app.flagged@gmail.com', // Change this to your email address
+      from: `"${fromEmail}" <${process.env.SMTP_USER}>`,
+      to: 'app.flagged@gmail.com',
       subject: "[Bug Report]" + subject,
       text: bugReport,
+    };
+
+    try {
+      await this.transporter.sendMail(mailOptions);
+      this.logger.log('Email sent successfully');
+      return { success: true, message: 'Email sent successfully' };
+    } catch (error) {
+      this.logger.error('Error sending email:', error);
+      throw new Error('Failed to send email');
+    }
+  }
+
+  // Send a contact us email
+  async sendContactUs(fromEmail: string, subject: string, message: string) {
+    this.logger.log('sendContactUs called with:', { fromEmail, subject, message }); // Log the input
+
+    const mailOptions = {
+      from: `"${fromEmail}" <${process.env.SMTP_USER}>`,
+      to: 'app.flagged@gmail.com',
+      subject: subject,
+      text: message,
     };
 
     try {
